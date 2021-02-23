1897a4ca4c188703f78bf33d69e7723111d36be3
unknown <mada.lawliet@gmail.com>
2021-02-23 16:57:53 +0700

    First Commit

---
+++ src/components/Sidebar.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+function Sidebar({ topAnime }) {
+  return (
+    <aside>
+      <nav>
+        <h3>Top Anime</h3>
+        {topAnime.map((anime) => (
+          <a href={anime.url} target="_blank" key={anime.mal_id} rel="noreffer">
+            {anime.title}
+          </a>
+        ))}
+      </nav>
+    </aside>
+  );
+}
+
+export default Sidebar;
